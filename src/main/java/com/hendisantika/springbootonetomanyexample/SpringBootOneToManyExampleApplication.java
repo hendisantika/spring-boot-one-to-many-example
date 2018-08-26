779e1d9cf51faec3ae76dfcb34390b4e598b3d6a
Hendi Santika <hendisantika@gmail.com>
2018-08-26 15:19:56 +0700

    #1 - Initialize project

---
+++ src/main/java/com/hendisantika/springbootonetomanyexample/SpringBootOneToManyExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootonetomanyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOneToManyExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOneToManyExampleApplication.class, args);
+    }
+}
